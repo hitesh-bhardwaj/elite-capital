d92cbd01b8872c8579426e1621fa7355f4ec3337
Vidushi Saxena <vidushisaxenabdn786@gmail.com>
2025-03-25 15:13:22 +0530

    update

--- src/components/Contact/Location.jsx
+++ src/components/Contact/Location.jsx
@@ -6,9 +6,9 @@ const Location = () => {
     const { t } = useTranslation('contact');
     return (
         <>
-            <section className="relative min-h-screen bg-[#747977] py-[3vw] overflow-hidden mobile:py-[15vw] mobile:min-h-full mobile:h-full">
-                <div className="relative z-10 px-[5vw]  flex items-center justify-between h-screen mobile:flex-col-reverse mobile:gap-[10vw] mobile:h-full">
-                    <div className="w-[60%] flex h-full items-center justify-between mobile:w-full">
+            <section className="relative min-h-screen bg-[#747977] py-[3vw] overflow-hidden mobile:py-[15vw] mobile:min-h-full mobile:h-full tablet:min-h-full tablet:py-[10vw]">
+                <div className="relative z-10 px-[5vw]  flex items-center justify-between h-screen mobile:flex-col-reverse mobile:gap-[10vw] mobile:h-full tablet:h-full">
+                    <div className="w-[60%] flex h-full items-center justify-between mobile:w-full tablet:w-1/2">
                         <Image
                             className="w-[90%] fadeUp mobile:w-full"
                             src={locationMap}
@@ -17,14 +17,14 @@ const Location = () => {
                             loading="lazy"
                         />
                     </div>
-                    <div className="text-white w-[45%] mobile:w-full mobile:space-y-[5vw] ">
+                    <div className="text-white w-[45%] mobile:w-full mobile:space-y-[5vw] tablet:w-1/2">
                         <h2 data-title-anim className="heading-2  mb-[1.5vw]">{t('locationHead')}</h2>
                         <div className="flex items-start justify-start gap-[1vw] ">
                             <div className="pt-[0.5vw]">
                                 <Image src="/icons/location-icon.svg" height={30} width={30} alt="location" className="fadein"/>
                             </div>
                             <div data-para-anim
-                                className="mb-[3vw] w-[80%] content mobile:w-[75%] mobile:pl-[3vw]"
+                                className="mb-[3vw] w-[80%] content mobile:w-[75%] mobile:pl-[3vw] tablet:w-full"
                                 dangerouslySetInnerHTML={{ __html: t('locationSub') }}
                             ></div>
 
@@ -35,13 +35,13 @@ const Location = () => {
                                 <div className="">
                                     <Image src="/icons/phone-icon.svg" height={35} width={35} alt="phone" className="fadein" />
                                 </div>
-                                <p data-para-anim className=" w-[60%] content mobile:pl-[3vw] ">{t('phone')}</p>
+                                <p data-para-anim className=" w-[60%] content mobile:pl-[3vw] tablet:w-full">{t('phone')}</p>
                             </div>
                             <div className="flex items-center justify-start gap-[1vw] py-[0.5vw]  ">
                                 <div className="">
                                     <Image src="/icons/mail-icon.svg" height={35} width={35} alt="mail" className="fadein" />
                                 </div>
-                                <p data-para-anim className=" w-[60%] content mobile:pl-[3vw]">{t('mail')}</p>
+                                <p data-para-anim className=" w-[60%] content mobile:pl-[3vw] tablet:w-full">{t('mail')}</p>
 
                             </div>
                         </div>
