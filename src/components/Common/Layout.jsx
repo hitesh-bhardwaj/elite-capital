accc90ab5fcb93046509ae60bb0455512b05bec5
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2025-05-23 17:47:33 +0530

    update

--- src/components/Common/Layout.jsx
+++ src/components/Common/Layout.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import Footer from './Footer'
-import Header from './Header'
+import React, { useEffect } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import {
+  ImageObjectJsonLd,
+  LocalBusiness,
+  OrganizationJsonLd,
+  WebsiteJsonLd,
+} from "@/lib/json-ld";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  const { locale } = useRouter();
+
+  useEffect(() => {
+    const dir = locale === "ar" ? "rtl" : "ltr";
+    document.documentElement.setAttribute("dir", dir);
+    document.documentElement.setAttribute("lang", locale);
+    document.querySelectorAll("Swiper").forEach((swiper) => {
+      swiper.setAttribute("dir", dir);
+    });
+  }, [locale]);
+
   return (
     <>
+      <LocalBusiness />
+      <OrganizationJsonLd />
+      <WebsiteJsonLd />
+      <ImageObjectJsonLd />
       <Header />
-        {children}
+      {children}
       <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Layout;
